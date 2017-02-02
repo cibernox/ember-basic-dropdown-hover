b8c9db11a5231fdafc410d6aeb52dc1049ea7df9
cibernox <miguel.camba@gmail.com>
2017-02-02 13:19:16 +0000

    Invoke content for of basic-dropdown

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-basic-dropdown-hover'
+  name: 'ember-basic-dropdown-hover',
+
+  contentFor(type, config) {
+    let emberBasicDropdown = this.addons.find((a) => a.name === 'ember-basic-dropdown');
+    return emberBasicDropdown.contentFor(type, config);
+  }
 };
